9cac9cbf531f8be6d44ea5636cf82344ff3d7bb4
jvsena42 <jvsena16@gmail.com>
2020-04-29 21:56:01 -0300

    Cadastrando usuario

--- app/src/main/java/com/balatech/uber/activity/CadastroActivity.java
+++ app/src/main/java/com/balatech/uber/activity/CadastroActivity.java
@@ -1,18 +1,28 @@
 package com.balatech.uber.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.Switch;
 import android.widget.Toast;
 
 import com.balatech.uber.R;
+import com.balatech.uber.config.ConfiguracaoFirebase;
+import com.balatech.uber.model.Usuario;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class CadastroActivity extends AppCompatActivity {
 
     private EditText campoNome, campoEmail, campoSenha;
     private Switch switchTipoUsuario;
+
+    private FirebaseAuth autenticacao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,7 +36,7 @@ public class CadastroActivity extends AppCompatActivity {
         switchTipoUsuario = findViewById(R.id.switchTipoUsuario);
     }
 
-    public void validarCadastroUsuario(){
+    public void validarCadastroUsuario(View view){
 
         //Recuperar textos dos campos
         String textoNome = campoNome.getText().toString();
@@ -35,10 +45,39 @@ public class CadastroActivity extends AppCompatActivity {
 
         if (!textoNome.isEmpty() && !textoEmail.isEmpty() && !textoSenha.isEmpty()){
 
+            Usuario usuario = new Usuario();
+            usuario.setNome(textoNome);
+            usuario.setEmail(textoEmail);
+            usuario.setSenha(textoSenha);
+            usuario.setTipo(verificaTipoUsuario());
+
+            cadastrarUsuario(usuario);
+
         }else {
             Toast.makeText(this, "Preencha todos os campos!", Toast.LENGTH_SHORT).show();
         }
+    }
 
+    public void cadastrarUsuario(Usuario usuario){
+
+        autenticacao = ConfiguracaoFirebase.getFirebaseAuth();
+        autenticacao.createUserWithEmailAndPassword(
+                usuario.getEmail(),
+                usuario.getSenha()
+        ).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    Toast.makeText(CadastroActivity.this, "Cadastrado oom sucesso!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
     }
+
+    public String verificaTipoUsuario(){
+        return switchTipoUsuario.isChecked() ? "M" : "P";
+    }
+
+
 }
