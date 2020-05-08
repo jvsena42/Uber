271c45052d563aa1c0964dfd2c8b2f3796bdc47a
jvsena42 <jvsena16@gmail.com>
2020-05-07 22:08:45 -0300

    Recuperando localização do motorista

---
+++ app/src/main/java/com/balatech/uber/activity/CorridaActivity.java
@@ -0,0 +1,125 @@
+package com.balatech.uber.activity;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+import com.balatech.uber.R;
+
+public class CorridaActivity extends AppCompatActivity
+            implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+    private LatLng localMotorista;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_corrida);
+
+        inicializarComponentes();
+    }
+
+    public void aceitarCorrida(View view){
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        //Recuperar localizacao do usuário
+        recuperarLocalizacaoUsuario();
+
+    }
+
+    private void recuperarLocalizacaoUsuario() {
+
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+                //Recuperar latitude e longitude
+                double latitude = location.getLatitude();
+                double longitude = location.getLongitude();
+                localMotorista = new LatLng(latitude, longitude);
+
+                mMap.clear();
+                mMap.addMarker(
+                        new MarkerOptions()
+                                .position(localMotorista)
+                                .title("Meu Local")
+                                .icon(BitmapDescriptorFactory.fromResource(R.drawable.carro))
+                );
+                mMap.moveCamera(
+                        CameraUpdateFactory.newLatLngZoom(localMotorista, 20)
+                );
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+        //Solicitar atualizações de localização
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ) {
+            locationManager.requestLocationUpdates(
+                    locationManager.GPS_PROVIDER,
+                    10000,
+                    10,
+                    locationListener
+            );
+        }
+
+    }
+
+    private void inicializarComponentes(){
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Iniciar Corrida");
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+}
--- app/src/main/java/com/balatech/uber/activity/RequisicoesActivity.java
+++ app/src/main/java/com/balatech/uber/activity/RequisicoesActivity.java
@@ -5,18 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.TextView;
 
 import com.balatech.uber.R;
 import com.balatech.uber.adapter.RequisicoesAdapter;
 import com.balatech.uber.config.ConfiguracaoFirebase;
+import com.balatech.uber.helper.RecyclerItemClickListener;
 import com.balatech.uber.helper.UsuarioFirebase;
 import com.balatech.uber.model.Requisicao;
 import com.balatech.uber.model.Usuario;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -39,12 +52,17 @@ public class RequisicoesActivity extends AppCompatActivity {
     private RequisicoesAdapter requisicoesAdapter;
     private Usuario motorista;
 
+    private LocationManager locationManager;
+    private LocationListener locationListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_requisicoes);
 
         configuracoesIniciais();
+        recuperarLocalizacaoUsuario();
+
     }
 
 
@@ -87,6 +105,32 @@ public class RequisicoesActivity extends AppCompatActivity {
         recyclerRequisicoes.setLayoutManager(layoutManager);
         recyclerRequisicoes.setAdapter(requisicoesAdapter);
 
+        //Adicionar evento de clique no recyclerview
+        recyclerRequisicoes.addOnItemTouchListener(
+                new RecyclerItemClickListener(getApplicationContext(),
+                        recyclerRequisicoes,
+                        new RecyclerItemClickListener.OnItemClickListener() {
+                            @Override
+                            public void onItemClick(View view, int position) {
+                                Requisicao requisicao = listRequisicao.get(position);
+                                Intent i = new Intent(RequisicoesActivity.this,CorridaActivity.class);
+                                i.putExtra("idRequisicao",requisicao.getId());
+                                i.putExtra("motorista",motorista);
+                                startActivity(i);
+                            }
+
+                            @Override
+                            public void onLongItemClick(View view, int position) {
+
+                            }
+
+                            @Override
+                            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                            }
+                        })
+        );
+
         recuperarRequisicoes();
     }
 
@@ -120,6 +164,55 @@ public class RequisicoesActivity extends AppCompatActivity {
 
             }
         });
+
+    }
+
+    private void recuperarLocalizacaoUsuario() {
+
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+
+                //Recuperar latitude e longitude
+                String latitude = String.valueOf(location.getLatitude());
+                String longitude = String.valueOf(location.getLongitude());
+
+                if (!latitude.isEmpty() && !longitude.isEmpty()){
+                    motorista.setLatitude(latitude);
+                    motorista.setLongitude(longitude);
+                    locationManager.removeUpdates(locationListener);
+                    requisicoesAdapter.notifyDataSetChanged();
+                }
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        };
+
+        //Solicitar atualizações de localização
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ) {
+            locationManager.requestLocationUpdates(
+                    locationManager.GPS_PROVIDER,
+                    0,
+                    0,
+                    locationListener
+            );
+        }
 
     }
 }
